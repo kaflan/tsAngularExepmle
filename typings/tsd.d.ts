f3aedd901518fbeb3c9e9d72baa7def086360831
kaflan <kaf_90@mail.ru>
2016-05-09 18:43:04 +0300

    typings angular js

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,6 +2,7 @@
  * Created by kaflan on 09.05.16.
  */
 /// <reference path="angularjs/angular.d.ts" />
+/// <reference path="angular-ui-router/angular-ui-router.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
 
 declare function require(string): any;
