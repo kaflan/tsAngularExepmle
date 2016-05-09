4bae7f2cf120fc0caf0f10a4058fb8d68809f799
kaflan <kaf_90@mail.ru>
2016-05-09 18:09:44 +0300

    delete .idea

--- generator/component/temp.ts
+++ generator/component/temp.ts
@@ -1,3 +1,4 @@
+/// <reference path="../../typings/angularjs/angular.d.ts" />
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import <%= name %>Component from './<%= name %>.component';
